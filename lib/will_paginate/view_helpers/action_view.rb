ef4e66c2d68a5d1455b993ab82a2599dda6362ca
Josh Delsman <jdelsman@voxxit.com>
2010-07-18 01:36:55 -0400

    Pagination links need to be html_safe, or the entire block will be returned as raw, escaped HTML

--- lib/will_paginate/view_helpers/action_view.rb
+++ lib/will_paginate/view_helpers/action_view.rb
@@ -63,7 +63,7 @@ module WillPaginate
       # apply. Don't use the <tt>:id</tt> option; otherwise you'll finish with two
       # blocks of pagination links sharing the same ID (which is invalid HTML).
       def paginated_section(*args, &block)
-        pagination = will_paginate(*args).to_s
+        pagination = will_paginate(*args).to_s.html_safe
         pagination + capture(&block) + pagination
       end
       
